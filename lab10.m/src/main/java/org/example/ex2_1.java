39a9d05e427413b3cb8e30ba2138d9c0bcb4a44a
vex <thewitchofthenewsun@gmail.com>
2024-01-15 19:01:44 +0200

    lab 10

---
+++ lab10.m/src/main/java/org/example/ex2_1.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.io.File;  // Import the File class
+
+public class ex2_1 {
+    public static void main(String[] args) {
+        File myObj = new File("filename.txt");
+        if (myObj.exists()) {
+            System.out.println("File name: " + myObj.getName());
+            System.out.println("Absolute path: " + myObj.getAbsolutePath());
+            System.out.println("Writeable: " + myObj.canWrite());
+            System.out.println("Readable " + myObj.canRead());
+            System.out.println("File size in bytes " + myObj.length());
+        } else {
+            System.out.println("The file does not exist.");
+        }
+    }
+}
